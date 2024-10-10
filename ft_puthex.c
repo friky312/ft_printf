d596f6c09a6e91647803ae94bb90310b93da7a15
friky312 <165424672+friky312@users.noreply.github.com>
2024-10-10 12:58:14 +0200

    ft_puthex.c

--- ft_puthex.c
+++ ft_puthex.c
@@ -3,7 +3,7 @@
 int	ft_puthex(unsigned int n, int lower)
 {
 	char	*numbers;
-	char	buffer[9];
+	char	buffer[9];  //9 es el tamaño máximo de un entero sin signo, Cuando representas este valor en hexadecimal, el número más grande que puedes obtener es FFFFFFFF (8 F)
 	int		count;
 	int		i;
 
@@ -17,7 +17,7 @@ int	ft_puthex(unsigned int n, int lower)
 		return (ft_putchar('0'));
 	while (n > 0)
 	{
-		buffer[i++] = numbers[n % 16];
+		buffer[i++] = numbers[n % 16];  //te da el dígito hexadecimal correspondiente al valor de n%16, lo guarda en i
 		n /= 16;
 	}
 	while (i > 0)
@@ -30,19 +30,19 @@ int	ft_puthex(unsigned int n, int lower)
 
 int main(void) 
 {
-    ft_puthex(42, 1);
+    ft_puthex(42, 1);  //2a
     printf("\n");
-    ft_puthex(42, 0);
+    ft_puthex(42, 0);  //2A
     printf("\n");
-    ft_puthex(-42, 1);
+    ft_puthex(-42, 1);  //ffffffd6
     printf("\n");
-    ft_puthex(-42, 0);
+    ft_puthex(-42, 0);  //FFFFFFD6
     printf("\n");
-    ft_puthex('a', 1);
+    ft_puthex('a', 1);  //61
     printf("\n");
-    ft_puthex('a', 0);
+    ft_puthex('a', 0);  //61
     printf("\n");
-    ft_puthex('A', 1);
+    ft_puthex('A', 1);  //41
     printf("\n");
-    ft_puthex('A', 0);
+    ft_puthex('A', 0); //41
 }
